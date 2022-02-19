d5365e7c5065f5ed0e35d770642ef85f2200a4a3
keyur diwan <keyurdiwan009@gmail.com>
2022-02-19 10:25:48 +0530

    add uiValues for filtering and sorting

--- backend/controllers/student.controller.js
+++ backend/controllers/student.controller.js
@@ -8,12 +8,26 @@ exports.getAllStudents = asyncHandler( async ( req, res, next ) => {
     // Add Filter Functionality
     let query;
 
+    let uiValues = {
+        filtering: {},
+        sorting: {},
+    }
+
     const reqQuery = { ...req.query };
 
     const removeFields = ['select', 'sort', 'page', 'limit'];
 
+    
     removeFields.forEach( val => delete reqQuery[val] );
 
+    // make an array of all the keys we receive in query
+    const filterKeys = Object.keys( reqQuery );
+    const filterValues = Object.values( reqQuery );
+
+    filterKeys.forEach( ( val, idx ) => uiValues.filtering[val] = filterValues[idx] );
+    
+
+    
     let queryStr = JSON.stringify( reqQuery );
 
     // Create query string
@@ -27,6 +41,18 @@ exports.getAllStudents = asyncHandler( async ( req, res, next ) => {
     if ( req.query.sort ) {
         
         const sortByArr = req.query.sort.split( ',' );
+
+        sortByArr.forEach( val => {
+            let order;
+            if ( val[0] === '-' ) {
+                order = 'descending';
+            } else {
+                order = 'ascending';
+            }
+
+            uiValues.sorting[value.replace( '-', '' )] = order;
+            
+        } );
         
         const sortByStr = sortByArr.join( ' ' );
         
@@ -48,9 +74,17 @@ exports.getAllStudents = asyncHandler( async ( req, res, next ) => {
     // Get All Students Data..!!
     const student = await query    // Student.find(JSON.parse(queryStr));
 
+    const maxMarks = await Student.find().sort({testMark: -1}).limit(1).select("-_id testMark");
+
+    const minMarks = await Student.find().sort({testMark: 1}).limit(1).select("-_id testMark");
+
+    uiValues.maxMarks = maxMarks[0].testMark;
+    uiValues.minMarks = minMarks[0].testMark;
+
     res.status( 200 ).json( {
         success: true,
         data: student,
+        uiValues,
     } );
 } );
 
